d5bd3d0760b0e221d7c5eb58dac67ac2a2594ccf
Enver Bisevac <enver.bisevac@harness.io>
2021-07-12 14:23:11 +0200

    versioning

--- setup.py
+++ setup.py
@@ -53,6 +53,5 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/drone/ff-python-server-sdk",
-    version="version='1.1.0'",
     zip_safe=False,
 )
