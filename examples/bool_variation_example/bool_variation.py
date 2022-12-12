654d4e47db062697d57791c02c5816a1daa6cd6e
Erdi Rowlands <erdi.rowlands@harness.io>
2022-12-12 18:14:13 +0000

    FFM-5995 tidy up

--- examples/bool_variation_example/bool_variation.py
+++ examples/bool_variation_example/bool_variation.py
@@ -4,24 +4,15 @@ from featureflags.evaluations.auth_target import Target
 from featureflags.client import CfClient
 from featureflags.util import log
 
-## TODO - undo this file
 def main():
     log.debug("Starting example")
-    api_key = "545535fb-43c0-448d-b57f-498c378c7a80"
+    api_key = "Your API Key"
     client = CfClient(api_key)
 
-    target = Target(identifier='2', attributes={"email": "demo@harness.io"})
-    target2 = Target(identifier='3', attributes={"email": "demo@harness.io"})
-    target3 = Target(identifier='4', attributes={"email": "demo@harness.io"})
-    target4 = Target(identifier='5', attributes={"email": "demo@harness.io"})
+    target = Target(identifier='harness')
+
     while True:
-        result = client.bool_variation('flag1', target, False)
-        log.debug("Result %s", result)
-        result = client.bool_variation('flag1', target2, False)
-        log.debug("Result %s", result)
-        result = client.bool_variation('flag1', target3, False)
-        log.debug("Result %s", result)
-        result = client.bool_variation('flag1', target4, False)
+        result = client.bool_variation('identifier_of_your_bool_flag', target, False)
         log.debug("Result %s", result)
         time.sleep(10)
 
