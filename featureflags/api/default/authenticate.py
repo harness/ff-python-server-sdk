472b6e6d686c81fbd62dd715c24933d92ba0c31a
Erdi Rowlands <erdi.rowlands@harness.io>
2023-06-02 15:41:42 +0100

    FFM-7177 Fix retry handler

--- featureflags/api/default/authenticate.py
+++ featureflags/api/default/authenticate.py
@@ -85,9 +85,11 @@ def handle_http_result(response):
 def _post_request(kwargs, max_auth_retries):
     retryer = Retrying(
         wait=wait_exponential(multiplier=1, min=4, max=10),
-        retry=(retry_if_result(
-            lambda response: response.status_code != 200
-            and handle_http_result)),
+        retry=(
+            retry_if_result(
+                lambda response: response.status_code != 404)
+            and retry_if_result(handle_http_result)
+        ),
         before_sleep=lambda retry_state: log.warning(
             f'SDK_AUTH_2002: Authentication attempt #'
             f'{retry_state.attempt_number} '
