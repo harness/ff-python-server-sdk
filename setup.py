75be46f8222d6a2c2c26f169b40bb5f2855d122a
Erdi Rowlands <erdi.rowlands@harness.io>
2023-07-17 13:24:03 +0100

    FFM-8544 1.2.2 release prep

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/harness/ff-python-server-sdk",
-    version='1.2.1',
+    version='1.2.2',
     zip_safe=False,
 )
