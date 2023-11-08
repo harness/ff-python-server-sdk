4b7d4992eb0fde563c2bd76f41110c772099e125
Erdi Rowlands <erdirowlands@gmail.com>
2023-11-08 12:19:08 +0000

    FFM-9866 Change examples to use info level

--- examples/cleanup_example/cleanup.py
+++ examples/cleanup_example/cleanup.py
@@ -1,22 +1,27 @@
+import logging
 import time
 
 from featureflags.evaluations.auth_target import Target
 from featureflags.client import CfClient
 from featureflags.util import log
 
+
 def main():
-    log.debug("Starting example")
+    log.setLevel(logging.INFO)
+    log.info("Starting example")
     api_key = "Your API key"
     client = CfClient(api_key)
 
-    target = Target(identifier='HT_1', name="Harness_Target_1", attributes={"location": "emea"})
+    target = Target(identifier='HT_1', name="Harness_Target_1",
+                    attributes={"location": "emea"})
 
     for x in range(10):
         result = client.bool_variation('harnessappdemodarkmode', target, False)
-        log.debug("Result %s", result)
+        log.info("Result %s", result)
         time.sleep(10)
 
     client.close()
+
 
 if __name__ == "__main__":
     main()
