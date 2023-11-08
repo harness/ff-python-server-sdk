2eae8ca7d0bcdb9071fe6128ca6b61f560a5aba2
Erdi Rowlands <erdirowlands@gmail.com>
2023-11-08 11:29:08 +0000

    FFM-9866 Set default log level to WARNING

--- featureflags/util.py
+++ featureflags/util.py
@@ -3,4 +3,4 @@ import sys
 
 log = logging.getLogger(sys.modules[__name__].__name__)
 log.addHandler(logging.StreamHandler())
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARNING)
--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/harness/ff-python-server-sdk",
-    version='1.2.3',
+    version='1.2.4',
     zip_safe=False,
 )
