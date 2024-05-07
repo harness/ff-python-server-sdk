9fca2c895318a4d953901b0141ac670872d76bad
Erdi Rowlands <erdirowlands@gmail.com>
2024-05-07 13:27:42 +0100

    FFM-11385 Re-generate client API to bring in target-segment rules

--- featureflags/openapi/config/api/metrics/post_metrics.py
+++ featureflags/openapi/config/api/metrics/post_metrics.py
@@ -5,15 +5,18 @@ import httpx
 
 from ... import errors
 from ...client import AuthenticatedClient, Client
+from ...models.metrics import Metrics
 from ...types import UNSET, Response, Unset
 
 
 def _get_kwargs(
     environment_uuid: str,
-    target: str,
     *,
+    body: Metrics,
     cluster: Union[Unset, str] = UNSET,
 ) -> Dict[str, Any]:
+    headers: Dict[str, Any] = {}
+
     params: Dict[str, Any] = {}
 
     params["cluster"] = cluster
@@ -21,20 +24,33 @@ def _get_kwargs(
     params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
 
     _kwargs: Dict[str, Any] = {
-        "method": "get",
-        "url": "/client/env/{environment_uuid}/target/{target}/evaluations".format(
+        "method": "post",
+        "url": "/metrics/{environment_uuid}".format(
             environment_uuid=environment_uuid,
-            target=target,
         ),
         "params": params,
     }
 
+    _body = body.to_dict()
+
+    _kwargs["json"] = _body
+    headers["Content-Type"] = "application/json"
+
+    _kwargs["headers"] = headers
     return _kwargs
 
 
 def _parse_response(
     *, client: Union[AuthenticatedClient, Client], response: httpx.Response
 ) -> Optional[Any]:
+    if response.status_code == HTTPStatus.OK:
+        return None
+    if response.status_code == HTTPStatus.UNAUTHORIZED:
+        return None
+    if response.status_code == HTTPStatus.FORBIDDEN:
+        return None
+    if response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+        return None
     if client.raise_on_unexpected_status:
         raise errors.UnexpectedStatus(response.status_code, response.content)
     else:
@@ -54,17 +70,19 @@ def _build_response(
 
 def sync_detailed(
     environment_uuid: str,
-    target: str,
     *,
     client: AuthenticatedClient,
+    body: Metrics,
     cluster: Union[Unset, str] = UNSET,
 ) -> Response[Any]:
-    """Get feature evaluations for target
+    """Send metrics to the Analytics server.
+
+     Send metrics to Analytics server
 
     Args:
         environment_uuid (str):
-        target (str):
         cluster (Union[Unset, str]):
+        body (Metrics):
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
@@ -76,7 +94,7 @@ def sync_detailed(
 
     kwargs = _get_kwargs(
         environment_uuid=environment_uuid,
-        target=target,
+        body=body,
         cluster=cluster,
     )
 
@@ -89,17 +107,19 @@ def sync_detailed(
 
 async def asyncio_detailed(
     environment_uuid: str,
-    target: str,
     *,
     client: AuthenticatedClient,
+    body: Metrics,
     cluster: Union[Unset, str] = UNSET,
 ) -> Response[Any]:
-    """Get feature evaluations for target
+    """Send metrics to the Analytics server.
+
+     Send metrics to Analytics server
 
     Args:
         environment_uuid (str):
-        target (str):
         cluster (Union[Unset, str]):
+        body (Metrics):
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
@@ -111,7 +131,7 @@ async def asyncio_detailed(
 
     kwargs = _get_kwargs(
         environment_uuid=environment_uuid,
-        target=target,
+        body=body,
         cluster=cluster,
     )
 
--- featureflags/openapi/config/models/authenticate_proxy_key_body.py
+++ featureflags/openapi/config/models/authenticate_proxy_key_body.py
@@ -3,28 +3,43 @@ from typing import Any, Dict, List, Type, TypeVar
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
-T = TypeVar("T", bound="ErrorDetails")
+T = TypeVar("T", bound="AuthenticateProxyKeyBody")
 
 
 @_attrs_define
-class ErrorDetails:
-    """Additional details about the error"""
+class AuthenticateProxyKeyBody:
+    """
+    Attributes:
+        proxy_key (str):  Example: 896045f3-42ee-4e73-9154-086644768b96.
+    """
 
+    proxy_key: str
     additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
+        proxy_key = self.proxy_key
+
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "proxyKey": proxy_key,
+            }
+        )
 
         return field_dict
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        error_details = cls()
+        proxy_key = d.pop("proxyKey")
 
-        error_details.additional_properties = d
-        return error_details
+        authenticate_proxy_key_body = cls(
+            proxy_key=proxy_key,
+        )
+
+        authenticate_proxy_key_body.additional_properties = d
+        return authenticate_proxy_key_body
 
     @property
     def additional_keys(self) -> List[str]:
