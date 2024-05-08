0206844e1d397614cdea27e24640cf4867335d91
Erdi Rowlands <erdirowlands@gmail.com>
2024-05-08 12:55:38 +0100

    FFM-11385 Logg retry attempt

--- featureflags/retryable_request.py
+++ featureflags/retryable_request.py
@@ -3,7 +3,8 @@ from tenacity import retry_if_result, wait_exponential, \
 from http import HTTPStatus
 from typing import Any, Optional, Union, List
 
-from .openapi.config.api.client.authenticate import sync_detailed as authenticate
+from .openapi.config.api.client.authenticate import \
+    sync_detailed as authenticate
 from .openapi.config.api.client.get_all_segments import \
     sync_detailed as retrieve_segments
 from .openapi.config.api.client.get_feature_config import \
@@ -13,14 +14,23 @@ from .openapi.config.api.client.get_feature_config_by_identifier import \
 from .openapi.config.api.client.get_segment_by_identifier import \
     sync_detailed as retrieve_segment_by_identifier
 from .openapi.config.models import AuthenticationRequest, \
-    AuthenticationResponse
+    AuthenticationResponse, FeatureConfig
 
 from featureflags.openapi.config import AuthenticatedClient, Client
-from featureflags.openapi.config.types import Unset, UNSET
+from featureflags.openapi.config.types import Unset, UNSET, Response
 from .openapi.config.errors import UnexpectedStatus
 from .openapi.config.models import Segment, FeatureConfig
+from .sdk_logging_codes import warn_auth_retying
 
 MAX_RETRY_ATTEMPTS = 2
+
+
+class UnrecoverableAuthenticationException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return f"UnrecoverableAuthenticationException: {self.message}"
 
 
 def default_retry_strategy(before_sleep_func=None):
@@ -29,19 +39,22 @@ def default_retry_strategy(before_sleep_func=None):
             retry_if_result(
                 lambda response: response.status_code in [
                     HTTPStatus.BAD_GATEWAY, HTTPStatus.NOT_FOUND,
-                    HTTPStatus.INTERNAL_SERVER_ERROR, UnexpectedStatus, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED])),
+                    HTTPStatus.INTERNAL_SERVER_ERROR, UnexpectedStatus,
+                    HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED])),
         wait=wait_exponential(multiplier=1, max=10),
         before_sleep=before_sleep_func,
-        stop=stop_after_attempt(MAX_RETRY_ATTEMPTS)
+        stop=stop_after_attempt(MAX_RETRY_ATTEMPTS),
     )
 
 
-@default_retry_strategy()
+@default_retry_strategy(lambda retry_state: warn_auth_retying(
+    retry_state.attempt_number,
+    retry_state.outcome.result()))
 def retryable_authenticate(
         client: Union[AuthenticatedClient, Client],
         body: AuthenticationRequest) -> Optional[
     Union[Any, AuthenticationResponse]]:
-    response =  authenticate(client=client, body=body)
+    response = authenticate(client=client, body=body)
     return response
 
 
@@ -59,7 +72,7 @@ def retryable_retrieve_segments(environment_uuid: str,
 def retryable_retrieve_feature_config(environment_uuid: str,
                                       client: AuthenticatedClient,
                                       cluster: Union[Unset, str] = UNSET) -> \
-        Optional[List["FeatureConfig"]]:
+        Response[list[FeatureConfig]]:
     return retrieve_flags(client=client,
                           environment_uuid=environment_uuid,
                           cluster=cluster)
@@ -72,7 +85,7 @@ def retryable_retrieve_feature_config_by_identifier(environment_uuid: str,
                                                     AuthenticatedClient,
                                                     cluster: Union[
                                                         Unset, str] = UNSET) \
-        -> Optional[FeatureConfig]:
+        -> Response[FeatureConfig]:
     return retrieve_flag_by_identifier(client=client,
                                        identifier=identifier,
                                        environment_uuid=environment_uuid,
