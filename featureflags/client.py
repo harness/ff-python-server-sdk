fc73ba4f9b3838966dd5d9828459b57ab1e1bdf1
Conor Murray <conor.murray@harness.io>
2025-01-23 12:58:30 +0000

    chore: bump version to 1.7.1

--- featureflags/client.py
+++ featureflags/client.py
@@ -23,7 +23,7 @@ from .api import retryable_authenticate, \
 from .streaming import StreamProcessor
 from .util import log
 
-VERSION: str = "1.7.0"
+VERSION: str = "1.7.1"
 
 
 class MissingOrEmptyAPIKeyException(Exception):
